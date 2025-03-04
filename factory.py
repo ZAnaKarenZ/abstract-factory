563ca1d1aa9e4795a5460c2cde923eb869cd2b30
ZAnaKarenZ <akzm2004@icloud.com>
2025-03-03 19:00:50 -0600

    Added wrong file

--- factory.py
+++ factory.py
@@ -21,38 +21,3 @@ class ElegantClothingFactory(ClothingFactory):
         pass
     def createDress(self):
         pass
-
-#Abstract class
-class Biome:
-    @abstractmethod
-    def getAnimals(self):
-        pass
-    def ambientSound(self):
-        for animal in self.getAnimals():
-            animal.makeSound()
-
-#Concrete classes
-class TemperateDeciduousForest(Biome):
-    def getAnimals(self):
-        return [Bird(), Fish()]
-
-class CoralReef(Biome):
-    def getAnimals(self):
-        return [Fish()]
-
-
-#Main function
-def main():
-    #Forest
-    print("Forest sounds:")
-    forest = TemperateDeciduousForest()
-    forest.ambientSound()
-
-    #Coral reef
-    print("Coral reef sounds:")
-    reef = CoralReef()
-    reef.ambientSound()
-
-
-if __name__ == "__main__":
-    main()
