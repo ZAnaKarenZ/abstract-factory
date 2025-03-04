38e8bb2cb1bf55e1b89bfce7e407718992b69b31
ZAnaKarenZ <akzm2004@icloud.com>
2025-03-03 19:00:03 -0600

    Factories

--- factory.py
+++ factory.py
@@ -1,1 +1,58 @@
-print("Successful commit")
+from abc import ABC, abstractmethod
+
+#Abstract interface
+class ClothingFactory:
+    @abstractmethod
+    def createOutfit(self):
+        pass
+    @abstractmethod
+    def createDress(self):
+        pass
+
+#Concrete classes
+class CasualClothingFactory(ClothingFactory):
+    def createOutfit(self):
+        pass
+    def createDress(self):
+        pass
+
+class ElegantClothingFactory(ClothingFactory):
+    def createOutfit(self):
+        pass
+    def createDress(self):
+        pass
+
+#Abstract class
+class Biome:
+    @abstractmethod
+    def getAnimals(self):
+        pass
+    def ambientSound(self):
+        for animal in self.getAnimals():
+            animal.makeSound()
+
+#Concrete classes
+class TemperateDeciduousForest(Biome):
+    def getAnimals(self):
+        return [Bird(), Fish()]
+
+class CoralReef(Biome):
+    def getAnimals(self):
+        return [Fish()]
+
+
+#Main function
+def main():
+    #Forest
+    print("Forest sounds:")
+    forest = TemperateDeciduousForest()
+    forest.ambientSound()
+
+    #Coral reef
+    print("Coral reef sounds:")
+    reef = CoralReef()
+    reef.ambientSound()
+
+
+if __name__ == "__main__":
+    main()
