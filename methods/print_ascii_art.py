702c2aa999b3cd96e5c94ca3dbb8c3b9566b1e06
ZAnaKarenZ <akzm2004@icloud.com>
2025-03-08 22:40:59 -0600

    Editing comments

--- methods/print_ascii_art.py
+++ methods/print_ascii_art.py
@@ -7,6 +7,6 @@ def print_ascii_art(file):
     current_file_path = os.path.dirname(os.path.abspath(__file__))
     new_file_path = os.path.join(current_file_path, '..', 'props', file)
 
-    #Get absolute file path
+    #Open, read and print file
     f= open(new_file_path,'r')
     print(''.join([line for line in f]))
